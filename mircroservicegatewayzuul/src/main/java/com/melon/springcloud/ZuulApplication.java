06b9a901fc7e4d17d88839fef700ffe3fea2cacb
actangle <674253854@qq.com>
2019-08-04 23:12:32 +0800

    zuul非配置类demo

---
+++ mircroservicegatewayzuul/src/main/java/com/melon/springcloud/ZuulApplication.java
@@ -0,0 +1,21 @@
+package com.melon.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @ClassName ZuulApplication
+ * @Description
+ * @Author melon
+ * @Date 2019-08-04 22:32
+ * @Version
+ */
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class,args);
+    }
+}
