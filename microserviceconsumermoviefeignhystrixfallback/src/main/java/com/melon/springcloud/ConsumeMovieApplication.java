c351bb635c8921d033904df23d1b2a6f82355e5c
actangle <674253854@qq.com>
2019-08-04 14:07:40 +0800

    使用feign结合hystrix来实现方法回退

---
+++ microserviceconsumermoviefeignhystrixfallback/src/main/java/com/melon/springcloud/ConsumeMovieApplication.java
@@ -0,0 +1,30 @@
+package com.melon.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @ClassName ConsumeMovieApplication
+ * @Description
+ * @Author melon
+ * @Date 2019-07-23 12:10
+ * @Version
+ */
+
+@SpringBootApplication
+@EnableFeignClients
+public class ConsumeMovieApplication {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumeMovieApplication.class, args);
+    }
+
+}
