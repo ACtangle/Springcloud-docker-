3cecad7d2d9765eb34fcb67ef5d5d8bef0dc5914
actangle <674253854@qq.com>
2019-08-04 14:36:42 +0800

    添加hystrix依赖，虽然feign包含hystrix但是缺失对hystrix.stream的监控流依赖，所以得引入依赖，并在启动类添加注解@EnableCircuitBreaker

--- microserviceconsumermoviefeignhystrixfallback/src/main/java/com/melon/springcloud/ConsumeMovieApplication.java
+++ microserviceconsumermoviefeignhystrixfallback/src/main/java/com/melon/springcloud/ConsumeMovieApplication.java
@@ -2,6 +2,7 @@ package com.melon.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -16,6 +17,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableCircuitBreaker
 public class ConsumeMovieApplication {
 
     @Bean
